e652feca691455fb22b2e8b2c1168ce447e919d0
serine000 <serine_md@hotmail.com>
2022-12-17 15:31:20 +0200

    Added producer & all container in one docker-compose file

---
+++ data_producer.py
@@ -0,0 +1,35 @@
+import os
+import time
+import json
+import random
+from datetime import datetime
+from data_generator import generate_message
+from kafka import KafkaProducer
+
+
+# ENV variables
+kafka_topic = os.getenv('KAFKA_TOPIC')#'first_python_topic'
+
+
+# Serialize message as JSON
+def serializer(message):
+    return json.dumps(message).encode('utf-8')
+
+# Kafka Producer
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:29092'],
+    value_serializer=serializer
+)
+
+if __name__ == '__main__':
+    while True:
+        # Generate message 
+        dummy_message = generate_message()
+
+        # Send it to our topic
+        print(f'Producing message @ {datetime.now()} | Message = {str(dummy_message)}')
+        producer.send(kafka_topic, dummy_message)
+
+        # Sleep for a random number of seconds
+        time_to_sleep = random.randint(1, 11)
+        time.sleep(time_to_sleep)
